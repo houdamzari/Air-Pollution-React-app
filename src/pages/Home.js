9ecdfeeb236c42dc02d05f7730dbdf44e5d751fa
houdamzari <houdamzari1999@gmail.com>
2023-02-09 18:30:24 +0100

    Setup pages

---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+import { useSelector, useDispatch } from "react-redux";
+import { Grid, Container, SingleCity, Search } from "../components";
+import { useNavigate } from "react-router-dom";
+import { fetchAirPollution } from "../slices/aitpolutionSlice";
+function Home() {
+  const { cities, filteredCities, searchString } = useSelector(
+    (state) => state.cities
+  );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const displayCities = searchString ? filteredCities : cities;
+  const handleClick = (location, city) => {
+    dispatch(fetchAirPollution(location));
+    navigate(`/${city.name}`);
+  };
+  return (
+    <Container>
+      <Search />
+      <Grid>
+        {displayCities.map((city) => (
+          <SingleCity
+            handleClick={() =>
+              handleClick({ lat: city.latitude, lon: city.longitude }, city)
+            }
+            key={city.name}
+          >
+            <p>{"City Name: " + city.name}</p>
+            <p> {"Population: " + city.population}</p>
+          </SingleCity>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
+
+export default Home;
